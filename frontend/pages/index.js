adb0267cdf2223d8476a43add9d2a7434ef110c9
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-09-26 08:14:21 +0100

    basic pages setup

---
+++ frontend/pages/index.js
@@ -0,0 +1,16 @@
+import { Button, Alert } from 'reactstrap';
+
+const Index = () => {
+  return (
+    <div>
+      <div>
+        <Alert color='primary'>
+          Hello Project is strapi-next with Bootstrap
+        </Alert>
+        <Button color='primary'>Hello from nextjs</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
