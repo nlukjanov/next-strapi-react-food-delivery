5c45de057677f5d833f84c5d32ca01f4c4d6c5d6
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-04 09:29:30 +0100

    fix: add api url to auth

--- frontend/lib/auth.js
+++ frontend/lib/auth.js
@@ -3,6 +3,7 @@ import Router from "next/router";
 import Cookie from "js-cookie";
 import axios from "axios";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
 
 export const registerUser = (username, email, password) => {
   if (typeof window === "undefined") {
