e06c47cdd5baf30429f6ffadda40e9f993478e87
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-04 10:41:40 +0100

    feat: add context to app to handle auth

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,23 +1,50 @@
+import { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Layout from '../components/Layout';
-
+import Cookie from 'js-cookie';
+import fetch from 'isomorphic-fetch';
+import AppContext from '../context/AppContext';
 import withData from '../lib/apollo';
 
 const App = ({ Component, pageProps }) => {
+  const [user, setUser] = useState(null);
+  const token = Cookie.get('token');
+  useEffect(() => {
+    if (token) {
+      fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then(async (res) => {
+          if (!res.ok) {
+            Cookie.remove('token');
+            setUser(null);
+            return null;
+          }
+          const user = await res.json();
+          setUser(user);
+        });
+    }
+  }, [token]);
   return (
     <>
-      <Head>
-        <link
-          rel='stylesheet'
-          href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
-          integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
-          crossOrigin='anonymous'
-        />
-      </Head>
+      <AppContext.Provider
+        value={{ user: user, isAuthenticated: !!user, setUser: setUser }}
+      >
+        <Head>
+          <link
+            rel='stylesheet'
+            href='https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css'
+            integrity='sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm'
+            crossOrigin='anonymous'
+          />
+        </Head>
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </AppContext.Provider>
     </>
   );
 };
