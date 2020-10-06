9d10af582d9553848d57a798e9386215cd3cd68e
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-10-06 19:52:39 +0100

    chore: lint auth

--- frontend/lib/auth.js
+++ frontend/lib/auth.js
@@ -1,22 +1,23 @@
-import { useEffect } from "react";
-import Router from "next/router";
-import Cookie from "js-cookie";
-import axios from "axios";
+import React from 'react';
+import { useEffect } from 'react';
+import Router from 'next/router';
+import Cookie from 'js-cookie';
+import axios from 'axios';
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
 
 export const registerUser = (username, email, password) => {
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     return;
   }
   return new Promise((resolve, reject) => {
     axios
       .post(`${API_URL}/auth/local/register`, { username, email, password })
       .then((res) => {
-        Cookie.set("token", res.data.jwt);
+        Cookie.set('token', res.data.jwt);
 
         resolve(res);
-        Router.push("/");
+        Router.push('/');
       })
       .catch((error) => {
         reject(error);
@@ -25,7 +26,7 @@ export const registerUser = (username, email, password) => {
 };
 
 export const login = (identifier, password) => {
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     return;
   }
 
@@ -33,10 +34,10 @@ export const login = (identifier, password) => {
     axios
       .post(`${API_URL}/auth/local/`, { identifier, password })
       .then((res) => {
-        Cookie.set("token", res.data.jwt);
+        Cookie.set('token', res.data.jwt);
 
         resolve(res);
-        Router.push("/");
+        Router.push('/');
       })
       .catch((error) => {
         reject(error);
@@ -45,26 +46,26 @@ export const login = (identifier, password) => {
 };
 
 export const logout = () => {
-  Cookie.remove("token");
+  Cookie.remove('token');
   delete window.__user;
-  window.localStorage.setItem("logout", Date.now());
-  Router.push("/");
+  window.localStorage.setItem('logout', Date.now());
+  Router.push('/');
 };
 
 export const withAuthSync = (Component) => {
   const Wrapper = (props) => {
     const syncLogout = (event) => {
-      if (event.key === "logout") {
-        Router.push("/login");
+      if (event.key === 'logout') {
+        Router.push('/login');
       }
     };
 
     useEffect(() => {
-      window.addEventListener("storage", syncLogout);
+      window.addEventListener('storage', syncLogout);
 
       return () => {
-        window.removeEventListener("storage", syncLogout);
-        window.localStorage.removeItem("logout");
+        window.removeEventListener('storage', syncLogout);
+        window.localStorage.removeItem('logout');
       };
     }, []);
 
